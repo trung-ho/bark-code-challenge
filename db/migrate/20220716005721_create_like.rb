598190ba4080f0440070ed048588978b3f132d33
Michael Ho <chacharoti@gmail.com>
2022-07-15 21:06:36 -0400

    Create dogs table

---
+++ db/migrate/20220716005721_create_like.rb
@@ -0,0 +1,9 @@
+class CreateLike < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.references :user, index: true
+      t.references :dog, index: true
+      t.timestamps
+    end
+  end
+end
