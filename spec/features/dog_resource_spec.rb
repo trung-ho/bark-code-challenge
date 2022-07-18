d6764ebe9f025c7a07dc6f93dbd19988cb3ae323
Michael Ho <chacharoti@gmail.com>
2022-07-18 00:49:57 -0400

    Add spec for like dog

--- spec/features/dog_resource_spec.rb
+++ spec/features/dog_resource_spec.rb
@@ -1,30 +1,51 @@
 require 'rails_helper'
 
 describe 'Dog resource', type: :feature do
-  before do 
-    @user = create(:user)
-    login_as(@user, scope: :user)
-  end
-  it 'can create a profile' do
-    visit new_dog_path
-    fill_in 'Name', with: 'Speck'
-    fill_in 'Description', with: 'Just a dog'
-    click_button 'Create Dog'
-    expect(Dog.count).to eq(1)
+  context 'when user is owner' do
+    before do 
+      @user = create(:user)
+      login_as(@user, scope: :user)
+    end
+    it 'can create a profile' do
+      visit new_dog_path
+      fill_in 'Name', with: 'Speck'
+      fill_in 'Description', with: 'Just a dog'
+      click_button 'Create Dog'
+      expect(Dog.count).to eq(1)
+    end
+
+    it 'can edit a dog profile' do
+      dog = create(:dog, owner: @user)
+      visit edit_dog_path(dog)
+      expect(page).not_to have_button('Like')
+      fill_in 'Name', with: 'Speck'
+      click_button 'Update Dog'
+      expect(dog.reload.name).to eq('Speck')
+    end
+
+    it 'can delete a dog profile' do
+      dog = create(:dog, owner: @user)
+      visit dog_path(dog)
+      click_link "Delete #{dog.name}'s Profile"
+      expect(Dog.count).to eq(0)
+    end
   end
 
-  it 'can edit a dog profile' do
-    dog = create(:dog, owner: @user)
-    visit edit_dog_path(dog)
-    fill_in 'Name', with: 'Speck'
-    click_button 'Update Dog'
-    expect(dog.reload.name).to eq('Speck')
-  end
+  context 'when user is not owner' do
+    before do 
+      @user = create(:user)
+      @user2 = create(:user)
+      login_as(@user2, scope: :user)
+    end
 
-  it 'can delete a dog profile' do
-    dog = create(:dog, owner: @user)
-    visit dog_path(dog)
-    click_link "Delete #{dog.name}'s Profile"
-    expect(Dog.count).to eq(0)
+    it 'can Like dog' do
+      dog = create(:dog, owner: @user)
+      visit dog_path(dog)
+      expect(page).to have_button('Like')
+      click_button 'Like'
+      expect(Like.last.dog).to eq(dog)
+      visit dog_path(dog)
+      expect(page).to have_selector("p", text: "You liked this dog.")
+    end
   end
 end
